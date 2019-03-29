79743bfe1a238e608372ddad746e67e4ea3eed15
TheFubon <43171309+cssdrive@users.noreply.github.com>
2019-03-29 10:04:53 +0300

    Добавлен вывод массива

--- templates/index.php
+++ templates/index.php
@@ -1,5 +1,8 @@
 <? require_once('header.php'); ?>
 	
-	<!-- CONTENT -->
+	<!-- Вывод массива -->
+	<? foreach ($SELAP['page']['Алиас массива'] as $item){
+		echo $item['Алиас поля'];
+	} ?>
 	
 <? require_once('footer.php'); ?>
